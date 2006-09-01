bd358fc151cebdec91ded5ac330a301e539a172a
Elfic.One <Elfic.One@836692e0-411c-0410-a2eb-e75ffc060f84>
2006-09-01 06:08:17 +0000

    initial commit Visual C# Express Edition Solution and Project z80 cpu emulator class z80 cpu debug class CS80.cs Main class

---
+++ CS80/CS80.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS80 {
+
+    class CS80 {
+
+        //Entry Point
+        static public void Main(string[] Arguments) {
+        }
+    }
+}
